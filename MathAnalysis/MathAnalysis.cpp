60be23d45574a10223b5b5fdda424440748c22ea
RaseWay <raseway@mail.ru>
2023-10-12 01:41:30 +0500

    added half-division method for math analysis

---
+++ MathAnalysis/MathAnalysis.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <windows.h>
+
+double f(double x)
+{
+	return x * x * x * x - x * x * x - 2 * (x * x) + 3 * x - 3;
+}
+
+double root(double f(double), double a, double b, double eps)
+{
+	double c;
+	while ((b - a) > 2 * eps) //считаем пока не получим ответ в допустимой погрешности
+	{
+		c = (a + b) / 2; // делим интервал пополам
+		if ((f(a) * f(c)) > 0) // если при подстановке 'c' в функцию знак меняется, то мы заменяем первую границу на 'c'. иначе - вторую.
+		{
+			a = c;
+		}
+		else
+		{
+			b = c;
+		}
+	}
+	return c;
+}
+
+int halfdevmeth()
+{
+	double a, b, x, eps;
+	std::cout << "Введите границы интервала и погрешность (a, b, eps): \n";
+	std::cout << "a: ";
+	std::cin >> a;
+	std::cout << "b: ";
+	std::cin >> b;
+	if ((f(a) * f(b)) > 0)
+	{
+		std::cout << "Неверный интервал или отсутствуют корни на заданном интервале. Попробуйте снова через 3 секунды.\n";
+		Sleep(3000);
+		return 0;
+	}
+	else
+	{
+		std::cout << "eps: ";
+		std::cin >> eps;
+	}
+	x = root(f, a, b, eps);
+	std::cout << "x =" << x << std::endl;
+	return 0;
+}
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	std::cout << "Перед вами калькулятор различной херни\n";
+	Sleep(1000);
+	std::cout << "которая может впринципе существовать\n";
+	Sleep(1000);
+	std::cout << "но пока здесь только один метод.\n";
+	Sleep(1000);
+	std::cout << "ДОБРО ПОЖАЛОВАТЬ.\n";
+	int n = 1, num;
+	while (n == 1)
+	{
+		std::cout << "Нажмите 1 для рассчета корня уравнения методом половинного деления. Нажмите 2 для анекдота (в будущем добавятся методы). \n";
+		std::cin >> num;
+		switch (num)
+		{
+		case 1:
+			halfdevmeth();
+			break;
+		case 2:
+			std::cout << "Штирлиц бежал скачками. Потом он сделал рывок и резко свернул за угол. Качки пробежали мимо.\n";
+			break;
+		}
+		std::cout << "Продолжаем? Жмай 1, чтобы снова выбрать метод. Жмай 2, чтобы break.\n";
+		std::cin >> n;
+	}
+}
